12debc4aad42cc589f90e3fb4d7ae45a36711903
Nicolás <naquiroz@uc.cl>
2021-09-28 09:32:44 -0300

    feat(add-grid-view): rename dimension and add to_s

--- lib/cell.rb
+++ lib/cell.rb
@@ -10,14 +10,14 @@ class Cell
     @bombs_around = 0
   end
 
-  def reveal(grid_cells, number_of_lines)
+  def reveal(grid_cells, dimension)
     @revealed = true
     return unless bombs_around.zero?
 
-    flood_fill(grid_cells, number_of_lines)
+    flood_fill(grid_cells, dimension)
   end
 
-  def count_bombs_around(grid_cells, number_of_lines)
+  def count_bombs_around(grid_cells, dimension)
     if @is_bomb
       @bombs_around = -1
       return
@@ -26,11 +26,11 @@ class Cell
     total_bombs = 0
     (-1..1).each do |x_offset|
       neighbour_x = @pos_x + x_offset
-      next if neighbour_x.negative? || neighbour_x >= number_of_lines
+      next if neighbour_x.negative? || neighbour_x >= dimension
 
       (-1..1).each do |y_offset|
         neighbour_y = @pos_y + y_offset
-        next if neighbour_y.negative? || neighbour_y >= number_of_lines
+        next if neighbour_y.negative? || neighbour_y >= dimension
 
         neighbour = grid_cells[neighbour_x][neighbour_y]
         total_bombs += 1 if neighbour.is_bomb
@@ -39,22 +39,26 @@ class Cell
     @bombs_around = total_bombs
   end
 
-  def flood_fill(grid_cells, number_of_lines)
+  def flood_fill(grid_cells, dimension)
     (-1..1).each do |x_offset|
       neighbour_x = @pos_x + x_offset
-      next if neighbour_x.negative? || neighbour_x >= number_of_lines
+      next if neighbour_x.negative? || neighbour_x >= dimension
 
       (-1..1).each do |y_offset|
         neighbour_y = @pos_y + y_offset
-        next if neighbour_y.negative? || neighbour_y >= number_of_lines
+        next if neighbour_y.negative? || neighbour_y >= dimension
 
         neighbour = grid_cells[neighbour_x][neighbour_y]
-        neighbour.reveal(grid_cells, number_of_lines) unless neighbour.revealed
+        neighbour.reveal(grid_cells, dimension) unless neighbour.revealed
       end
     end
   end
 
   def symbol
     is_bomb ? 'X' : bombs_around.to_s
+  end
+
+  def to_s
+    revealed ? symbol : '░'
   end
 end
