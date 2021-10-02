5e2e7dc03800d4718c6506ae35e6339cdd110601
Cadagan <mcadagan@uc.cl>
2021-10-02 18:55:08 -0300

    feat(user-input): completed UI, Game and main class

--- lib/game.rb
+++ lib/game.rb
@@ -19,30 +19,12 @@ class Game
     @game_board = Grid.new(game_dimention, number_bombs)
   end
 
-  def game_display
-    puts @game_board.to_s
-    puts "Enter coordinates for your guess separated by a comma (example: '2,3')"
-    puts "Enter 'exit' to finish the game"
-    make_guess
-  end
-
-  def make_guess
-    guess = gets.chomp
-    if guess == 'exit'
-      @game_in_progress = true
-    else
-      guess = guess.split(',')
-      if check_user_input(guess)
-        guess = guess.map(&:to_i)
-        reveal_guess(guess)
-      end
-    end
-  end
-
   def check_exit_game(user_input)
-    return true if user_input == 'exit'
-
-    false
+    if user_input == 'exit'
+      @game_in_progress = false
+      return true
+    end
+    return false
   end
 
   def check_user_input(user_input)
@@ -54,7 +36,7 @@ class Game
       return false if element.to_i.to_s != extra || (element.to_i >= @game_dimention || element.to_i.negative?)
     end
 
-    user_input
+    user_input.map(&:to_i)
   end
 
   def check_valid_game_setup(game_dimention, number_of_bombs)
@@ -65,11 +47,21 @@ class Game
 
   def reveal_guess(user_guess)
     @game_board.cells[user_guess[0]][user_guess[1]].reveal(@game_board.cells, @game_dimention)
-    game_status_check
+    game_status_check(user_guess)
   end
 
-  def game_status_check
-    puts 'The idea here is to check the amount of remaining bombs :) work in progress'
+  def game_status_check(user_guess)
+    if @game_board.cells[user_guess[0]][user_guess[1]].is_bomb
+      @game_lost = true
+    else
+      @game_board.cells.each do |row|
+        row.each do |cell|
+          return true if !cell.is_bomb && !cell.revealed
+        end
+      end
+      @game_won = true
+    end
+    @game_in_progress = false
   end
 end
 
